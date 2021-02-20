dcedb600f2ccd43c1895f44bfb810c95005818f6
Benjamin Westburg <38194972+westbenj2020@users.noreply.github.com>
2021-02-20 13:54:56 -0500

    02_20_2021
    - Lab is finished; the code could use some clean-up and revision of naming conventions.

--- server.py
+++ server.py
@@ -23,23 +23,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # creates server s
             string = key.read(8)
             string1 = string
             cipher = DES.new(string.encode('utf-8'), DES.MODE_ECB)
-            data = client.recv(64)
-            receivedMsg = cipher.decrypt(data)
-            unpad(receivedMsg, BLOCK_SIZE)
-            decodedMsg = receivedMsg.decode('utf-8').strip()
+            dataHMAC = client.recv(64)
+            dataHMACE = dataHMAC  # copy for encrypted print
+            receivedHMAC = cipher.decrypt(dataHMAC)
+            dataMessage = client.recv(1024)
+            dataMessageE = dataMessage  # copy for encrypted print
+            unpaddedDataMsg = unpad(cipher.decrypt(dataMessage), BLOCK_SIZE)
+            decodedMsg = unpaddedDataMsg.decode('utf-8').strip()
             #  split into message and hmac and carry out lab 3 processes
-            print('\n******************')
-            print('received ciphertext is: {}'.format(data.decode('utf-8', 'ignore')))
-            print('received plaintext is: {}'.format(decodedMsg))
-            print('******************')
+            print('\n--- Receiver side ---')
+            print('received ciphertext is: {}'.format(dataHMACE.decode('utf-8', 'ignore')) +
+                  dataMessageE.decode('utf-8', 'ignore'))
+            print('received message is: {}'.format(decodedMsg))
+            print('received hmac is: {}'.format(receivedHMAC.decode()))
+            msgAuth = hmac.new(hmacKey.encode(), decodedMsg.encode(), hashlib.sha256)
+            stringer = msgAuth.hexdigest()
+            print('calculated hmac is: {}'.format(stringer))
+            if receivedHMAC.decode() == stringer:
+                print('HMAC Verified')
             plaintext1 = input('\nType message:\n\n')  # retrieves plaintext input from user
-            print('\nHi, this is server.')
-            print('******************')
-            plaintext1Encoded = plaintext1.encode('utf-8')
-            cipher1 = DES.new(string1.encode('utf-8'), DES.MODE_ECB)
-            msg = cipher1.encrypt(pad(plaintext1Encoded, BLOCK_SIZE))
-            print('key is: "{}"'.format(string1))
+            msgAuth2 = hmac.new(hmacKey.encode(), plaintext1.encode(), hashlib.sha256)
+            stringer = msgAuth2.hexdigest()
+            msgWithHMAC = stringer + plaintext1
+            msgWithHMACEncoded = msgWithHMAC.encode('utf-8')
+            print('\n--- Sender side ---')
+            msg = cipher.encrypt(pad(msgWithHMACEncoded, BLOCK_SIZE))  # encrypts plaintext
             client.sendall(msg)  # sends encrypted message to server
-            print('Sent plaintext is: {}'.format(plaintext1))
-            print('Sent ciphertext is: {}'.format(msg.decode('utf-8', 'ignore')))
-            print('******************')
+            print('Shared DES key is: {}'.format(string1))
+            print('Shared HMAC key is: {}'.format(hmacKey))
+            print('plain message is: {}'.format(plaintext1))
+            print('sender side HMAC is: {}'.format(msgAuth2.hexdigest()))
+            print('sent ciphertext is: {}\n'.format(msg.decode('utf-8', 'ignore')))
+            #  plaintext1Encoded = plaintext1.encode('utf-8')
+            #  cipher1 = DES.new(string1.encode('utf-8'), DES.MODE_ECB)
+            #  msg = cipher1.encrypt(pad(plaintext1Encoded, BLOCK_SIZE))
